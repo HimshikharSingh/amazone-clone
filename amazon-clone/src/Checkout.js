ccc55dc1d57ab304a820e1b58d1aed41af1eb901
Aditya <adikpote@gmail.com>
2022-10-20 05:31:53 +0530

    Modified checkout page and styling of checkout button

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from "react";
 import "./Checkout.css";
 
 function Checkout() {
   return (
     <div className="checkout">
       <div className="checkout__left">
-        <img 
-        className="checkout__ad"
-        src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/PrimeRewards/LP_Revamp/PC_Header_Banner._CB468631809_.jpg"
-        alt=""
+        <img
+          className="checkout__ad"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/PrimeRewards/LP_Revamp/PC_Header_Banner._CB468631809_.jpg"
+          alt=""
         />
         <div>
-            <h2 className="checkout__title">Your 
-            Shopping Basket</h2>
-
+          <h2 className="checkout__title">Your Shopping Basket</h2>
         </div>
       </div>
-      <div className="checkout__right">
-        <h2>The subtotal</h2>
+      <div className="subtotal">
+        <div className="checkout__right">
+          <h2>The subtotal</h2>
+        </div>
+
+        <button>Proceed to Checkout</button>
       </div>
     </div>
   );
